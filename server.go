2243947360bb7d125d63234e4f39a240c757d8fc
Tiago Carreira <tiago.g.carreira+git@gmail.com>
2023-04-17 23:52:22 -0300

    gh-actions + goreleaser

--- server.go
+++ server.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/tcarreira/api-server/api"
+)
+
+var (
+	Version = "dev"
+	commit  = ""
+	date    = ""
 )
 
 func main() {
@@ -20,7 +28,14 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "ok")
 	})
+	e.GET("/version", func(c echo.Context) error {
+		return c.String(http.StatusOK, fmt.Sprintln(Version, commit, date))
+	})
 
 	// Start server
-	e.Logger.Fatal(e.Start(":8888"))
+	port := "8888"
+	if p := os.Getenv("API_PORT"); p != "" {
+		port = p
+	}
+	e.Logger.Fatal(e.Start(":" + port))
 }
